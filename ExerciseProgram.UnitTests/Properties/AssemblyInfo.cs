957317e40b84e2b6ca367181dc1ed40a716edfd9
Auston Hefling <auston.hefling@outlook.com>
2018-10-16 10:42:07 -0400

    Add Refrences similar to how LAB1A had us do for SportsStore (that is the layout i will use)

---
+++ ExerciseProgram.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExerciseProgram.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExerciseProgram.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("34679e99-d295-4a86-97ed-45970e6ff647")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ExerciseProgram.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExerciseProgram.WebUI.MvcApplication" Language="C#" %>
