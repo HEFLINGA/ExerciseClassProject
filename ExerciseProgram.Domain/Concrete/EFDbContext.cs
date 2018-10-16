d8c1b9f2b153ee8aea800fed16d4be75992e366a
Auston Hefling <auston.hefling@outlook.com>
2018-10-16 11:29:05 -0400

    Add NuGet

---
+++ ExerciseProgram.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,10 @@
+﻿using ExerciseProgram.Domain.Entities;
+using System.Data.Entity;
+
+namespace ExerciseProgram.Domain.Concrete
+{
+    public class EFDbContext : System.Data.Entity.DbContext
+    {
+        public DbSet<Exercise> Exercises { get; set; }
+    }
+}
---
+++ ExerciseProgram.Domain/Concrete/EFExerciseRepository.cs
@@ -0,0 +1,16 @@
+﻿using ExerciseProgram.Domain.Abstract;
+using ExerciseProgram.Domain.Entities;
+using System.Collections.Generic;
+
+namespace ExerciseProgram.Domain.Concrete
+{
+    public class EFExerciseRepository : IExerciseRepository
+    {
+        private EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Exercise> Exercises
+        {
+            get { return context.Exercises; }
+        }
+    }
+}
