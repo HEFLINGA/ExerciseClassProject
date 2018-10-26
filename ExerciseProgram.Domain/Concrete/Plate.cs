a889e60724c61bfcc6d2e6d933b8a1849bbfa1d2
Auston Hefling <auston.hefling@outlook.com>
2018-10-26 09:33:28 -0400

    Successfully add plate calculation and displaying in TEST accuratly based on exerciseMax, which is based on calculation for individual sets.

---
+++ ExerciseProgram.Domain/Concrete/Plate.cs
@@ -0,0 +1,64 @@
+﻿using ExerciseProgram.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseProgram.Domain.Concrete
+{
+    public class Plate : IPlate
+    {
+        private readonly string FourtyFive = "45";
+        private readonly string TwentyFive = "25";
+        private readonly string Ten = "10";
+        private readonly string Five = "5";
+        private readonly string TwoPointFive = "2.5";
+
+        public string PlateWeight(int totalWeight)
+        {
+            totalWeight -= 45;
+            int totalOf45 = 0;
+            int totalOf25 = 0;
+            int totalOf10 = 0;
+            int totalOf5 = 0;
+            int totalOf2Point5 = 0;        
+
+            while (totalWeight != 0)
+            {
+                if (totalWeight >= 5)
+                {
+                    totalOf2Point5 += 2;
+                    totalWeight -= 5;
+                }
+                if(totalOf2Point5 >= 4)
+                {
+                    totalOf5 += 2;
+                    totalOf2Point5 = 0;
+                }
+                if (totalOf5 >= 4)
+                {
+                    totalOf10 += 2;
+                    totalOf5 = 0;
+                }
+                if (totalOf10 >= 4 && totalOf5 >= 2)
+                {
+                    totalOf25 += 2;
+                    totalOf10 = 0;
+                    totalOf5 = 0;
+                }
+                if (totalOf25 >= 2 && totalOf10 >= 4)
+                {
+                    totalOf45 += 2;
+                    totalOf25 -= 2;
+                    totalOf10 -= 4;
+                }
+            }
+                
+
+            string answer = $"{totalOf45}x{FourtyFive}, {totalOf25}x{TwentyFive}, {totalOf10}x{Ten}, {totalOf5}x{Five}, {totalOf2Point5}x{TwoPointFive}";
+
+            return $"{answer}";
+        }
+    }
+}
--- ExerciseProgram.Test/UnitTest.cs
+++ ExerciseProgram.Test/UnitTest.cs
@@ -16,12 +16,18 @@ namespace ExerciseProgram.Test
         public void Can_Do_Math_On_FiveByFive_Sets()
         {
             Calculation cal = new Calculation();
+            Exercise exercise1 = new Exercise
+            {
+                Name = "Super Fly",
+                ExerciseMax = 225,
+                CategoryID = 1
+            };
 
-            Assert.AreEqual(135, cal.WeightOnFiveByFiveSet(225, 1));
+            Assert.AreEqual(135, cal.WeightOnFiveByFiveSet(exercise1.ExerciseMax, 1));
             Assert.AreEqual(170, cal.WeightOnFiveByFiveSet(245, 2));
             Assert.AreEqual(210, cal.WeightOnFiveByFiveSet(265, 3));
-            Assert.AreEqual(205, cal.WeightOnFiveByFiveSet(225, 4));
-            Assert.AreEqual(225, cal.WeightOnFiveByFiveSet(225, 5));
+            Assert.AreEqual(205, cal.WeightOnFiveByFiveSet(exercise1.ExerciseMax, 4));
+            Assert.AreEqual(225, cal.WeightOnFiveByFiveSet(exercise1.ExerciseMax, 5));
         }
 
         [TestMethod]
@@ -38,6 +44,29 @@ namespace ExerciseProgram.Test
             Calculation cal = new Calculation();
 
             Assert.AreEqual(180, cal.WeightOnOneByEightSet(225));
+        }
+
+        [TestMethod]
+        public void Can_Display_Plate_Weight_In_String()
+        {
+            Plate plate = new Plate();
+            Calculation cal = new Calculation();
+            Exercise exercise1 = new Exercise
+            {
+                Name = "Overhead Press",
+                Description = "SuperHard",
+                CategoryID = 4,
+                ExerciseMax = 205
+            };
+            Exercise exercise2 = new Exercise
+            {
+                ExerciseMax = 305
+            };
+            
+            Assert.AreEqual("2x45, 2x25, 2x10, 0x5, 0x2.5"
+                , plate.PlateWeight(cal.WeightOnFiveByFiveSet(exercise1.ExerciseMax, 5)));
+            Assert.AreEqual("6x45, 0x25, 0x10, 0x5, 2x2.5"
+                , plate.PlateWeight(cal.WeightOnOneByThreeSet(exercise2.ExerciseMax)));
         }
     }
 }
