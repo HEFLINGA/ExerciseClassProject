eb7b9763a6123bff90d4cf23084d7953dcfa3543
Auston Hefling <auston.hefling@outlook.com>
2018-10-30 09:34:46 -0400

    Adjusting test

--- ExerciseProgram.Test/UnitTest.cs
+++ ExerciseProgram.Test/UnitTest.cs
@@ -1,11 +1,5 @@
-﻿using System.Collections.Generic;
-using ExerciseProgram.Domain.Concrete;
+﻿using ExerciseProgram.Domain.Concrete;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
-using System.Linq;
-using ExerciseProgram.Domain.DAL;
-using System.Data.Entity;
-using ExerciseProgram.WebUI.Controllers;
 
 namespace ExerciseProgram.Test
 {
