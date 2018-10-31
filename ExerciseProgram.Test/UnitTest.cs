6d8dcacda5dfaf31823967e8278efda95ab2f7c1
Auston Hefling <auston.hefling@outlook.com>
2018-10-31 13:58:11 -0400

    Operational

--- ExerciseProgram.Test/UnitTest.cs
+++ ExerciseProgram.Test/UnitTest.cs
@@ -108,27 +108,6 @@ namespace ExerciseProgram.Test
         }
 
         [TestMethod]
-        public void Can_Generate_Page_Links()
-        {
-            HtmlHelper myHelper = null;
-
-            PagingInfo pagingInfo = new PagingInfo
-            {
-                CurrentPage = 2,
-                TotalItems = 28,
-                ItemsPerPage = 10
-            };
-
-            Func<int, string> pageUrlDelegate = i => "Page" + i;
-
-            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
-
-            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>" 
-                + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>" 
-                + @"<a class=""btn btn-default"" href=""Page3"">3</a>", result.ToString());
-        }
-
-        [TestMethod]
         public void Can_Send_Pagination_View_Model()
         {
             Mock<IExerciseRepository> mock = new Mock<IExerciseRepository>();
